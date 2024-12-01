483b1dab5e9e41e3a3dfee7e1d4be54a5263c962
Jari Roossien <jariroossien@gmail.com>
2024-12-01 12:15:10 +0700

    Completed Day 1

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ My submissions for the 2024 Edition of Advent of Code!
 
 ## Current Output
 ```
+Day 1 challenge 1: 1197984. solved in 1ms
+Day 1 challenge 2: 23387399. solved in 0ms
 ```
--- src/nl/dizmizzer/aoc/days/Day01.java
+++ src/nl/dizmizzer/aoc/days/Day01.java
@@ -1,19 +1,52 @@
 package nl.dizmizzer.aoc.days;
 
+import java.util.*;
 
 public class Day01 extends Day {
+
+    PriorityQueue<Integer> l = new PriorityQueue<>();
+    PriorityQueue<Integer> r = new PriorityQueue<>();
+
     @Override
     public long solveOne() {
-        return 0;
+        int difference = 0;
+        while (!l.isEmpty() && !r.isEmpty()) {
+            difference += Math.abs(l.poll() - r.poll());
+        }
+        return difference;
     }
+
+    Map<Integer, Integer> countingMap = new HashMap<>();
+    List<Integer> leftList = new ArrayList<>();
 
     @Override
     public long solveTwo() {
-        return 0;
+        long sum = 0;
+        for (int i : leftList) {
+            sum += (long) i * countingMap.getOrDefault(i, 0);
+        }
+        return sum;
     }
 
     @Override
     public void setup() {
         super.setup();
+        for (String s : input) {
+            if (s.isEmpty()) continue;
+            String[] in = s.trim().split(" {3}");
+
+            int leftKey = Integer.parseInt(in[0]);
+            int rightKey = Integer.parseInt(in[1]);
+
+            // Priority queue for part 1
+            l.add(leftKey);
+            r.add(rightKey);
+
+            // Part 2
+            leftList.add(leftKey);
+            countingMap.put(rightKey, countingMap.getOrDefault(rightKey, 0) + 1);
+
+        }
     }
+
 }
